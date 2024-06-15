737ecc3c4477b9afabd1112a88733ba62a8178c4
Bich Ngoc Dev <2ndevdes@gmail.com>
2024-06-16 02:57:10 +0700

    update

---
+++ B2-xu-ly-chuoi.py
@@ -0,0 +1,31 @@
+# Biến đại diện cho vùng nhớ trên bộ nhớ ram
+
+# --> quy tắc đặt tên biến
+# - Nó sẽ là một N hoặc động từ có nghĩa
+# a = "nhân viên"
+
+# Gán giá trị
+# <tên biến> = <gia ti cua bien>
+
+# Kiểu dữ liệu
+# - kiểu nguyên (int: integer): vd: 1, 4, 2ooo
+# - kiểu nguyên dài (long): vd: 244444
+# - kiểu số thực (float): vD:  2.5555
+# - Kiểu số thực dài (double) : vd: 2.5555555555555555
+# - kiểu ký tự vd "a", "b"
+# chuỗi (string) vd: "string", "ngoc" --> không giới hạn
+# kiểu boolean --> 0 hoặc 1 --> True or False --> 01000111
+
+
+# name = "Bich Ngoc"
+# age = 22
+# sex = True
+# print (name,"-", age,"-", sex)
+
+
+name = input("Nhap ten: ")
+age = input("Nhap tuoi")
+sex = True
+print (name,"-", age,"-", sex)
+
+# 31.27 https://www.youtube.com/watch?v=zYzxFKwyuS0&list=PLauJtJjb0hD0C7MMVnha9-FocyvvvRvww&index=3
